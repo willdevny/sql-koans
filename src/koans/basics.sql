2330c6007e7815b53feeda089c1d8afddfd4da21
willdevny <willdevny12@gmail.com>
2022-02-01 10:40:28 -0500

    basics, filtering, update_delete

--- src/koans/basics.sql
+++ src/koans/basics.sql
@@ -2,22 +2,22 @@
 SELECT 1
 
 -- Meditate on lower-case queries
-_____ 1
+select 1
 
 -- Meditate on selecting all columns from a table
 select * from book
 
 -- Meditate on selecting one column (title) from a table
-select _____ from book
+select title from book
 
 -- Meditate on selecting some columns (title, available) from a table
-select _____, _____ from book
+select title, available from book
 
 -- Meditate on adding a new book
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
+values('harry potter', 'fantasy', 1999, 1)
 
 -- Meditate on adding two new books
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
-,('_____', '_____', _____, 1)
+values('harry potter', 'fantasy', 1999, 1),
+('lord of the rings', 'fantasy', 1925, 1)
